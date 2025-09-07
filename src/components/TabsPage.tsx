4a40c426a17f722927d851a7d61ef2484e6f0de3
Max Tovstopiatyi <maxrori228@gmail.com>
2025-09-07 16:20:26 +0300

    add task solution

---
+++ src/components/TabsPage.tsx
@@ -0,0 +1,37 @@
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsPage = ({ tabs }: Props) => {
+  const { tabId } = useParams();
+  const selectedTab = tabs.find(tab => tab.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            const isActive = tab.id === tabId;
+
+            return (
+              <li
+                data-cy="Tab"
+                key={tab.id}
+                className={isActive ? 'is-active' : ''}
+              >
+                <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
---
+++ src/components/TabsRouter.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Tab } from '../types/Tab';
+import { TabsPage } from './TabsPage';
+
+interface Props {
+  tabs: Tab[];
+}
+
+export const TabsRouter: React.FC<Props> = ({ tabs }) => {
+  return (
+    <Routes>
+      <Route path="/" element={<h1 className="title">Home Page</h1>} />
+      <Route path="/" element={<Navigate to="/" replace />} />
+      <Route path="/tabs">
+        <Route index element={<TabsPage tabs={tabs} />} />
+        <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
+      </Route>
+      <Route path="*" element={<h1 className="title">Page not found</h1>} />
+    </Routes>
+  );
+};
